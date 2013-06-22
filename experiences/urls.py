9dc46c9f4b836af0441ba6c76d8fdb1b1bb99037
Andrew Gaines <andrew.s.gaines@gmail.com>
2013-06-21 19:13:43 -0600

    Quick fix to keep private experiences from showing in generic views

--- experiences/urls.py
+++ experiences/urls.py
@@ -12,11 +12,11 @@ urlpatterns = patterns(
     url(r'^(?P<experience_id>\d+)/invite/$', 'invite'),
     url(r'^(?P<experience_id>\d+)/brief/$', 'brief'),
     url(r'^(?P<experience_id>\d+)/categorize/$', 'categorize'),
-    url(r'^freshest/$', ListView.as_view(queryset=Experience.objects.order_by('date_created')[:5], context_object_name='freshest_experiences', template_name='experiences/freshest.html'), name="freshest_experiences"),
+    url(r'^freshest/$', ListView.as_view(queryset=Experience.objects.exclude(is_public=False).order_by('date_created')[:5], context_object_name='freshest_experiences', template_name='experiences/freshest.html'), name="freshest_experiences"),
     url(r'^featured/$', 'featured', name='featured_experiences'),
     url(r'^(?P<experience_id>\d+)/narratives/', include('narratives.urls')),
     url(r'^(?P<experience_id>\d+)/gallery/$', 'gallery', name='exp-gallery-list'),
     url(r'^(?P<experience_id>\d+)/upload_photo/$', 'upload_photo'),
-    url(r'^all/$', ListView.as_view(queryset=Experience.objects.all(), context_object_name='all_experiences', template_name='experiences/all.html'), name="all_experiences"),
+    url(r'^all/$', ListView.as_view(queryset=Experience.objects.exclude(is_public=False), context_object_name='all_experiences', template_name='experiences/all.html'), name="all_experiences"),
     url(r'^create_experience/$', 'create', name='create_experience'),
 )
