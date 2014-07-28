682e9c83ad9a602e699c242a95e9de6000a51043
Andrew Gaines <andrew.s.gaines@gmail.com>
2014-07-27 19:16:08 -0600

    Reverted back to former narrative display on experience index. Emails sent of notification, ability to unsubscribe present (of course).

--- explorers/life.py
+++
@@ -1,9 +0,0 @@
-from life import environment, cosmic_chance, adapt, traits
-
-time = True
-you = Individual(life=cosmic_chance(), characteristics=traits()))
-
-while time:
-    from environment import settings
-    if you.life:
-
