d682f8ae4c917840fcf0d7e7fa803d21b08d56e0
interpopulus <andrew.s.gaines@gmail.com>
2015-08-09 16:49:27 -0600

    Deleted commented urls

--- photologue/urls.py
+++ photologue/urls.py
@@ -27,15 +27,8 @@ urlpatterns = patterns(
         name='pl-gallery-archive'),
     url(r'^gallery/(?P<gallery_id>\d+)/gallery_edit/$', 'photologue.views.gallery_edit', name='pl-gallery-edit'),
     url(r'^gallery/(?P<gallery_id>\d+)/upload_photo/$', 'photologue.views.upload_photo'),
-    # Following was photologue written url:
-    # url(r'^gallery/(?P<pk>[\-\d\w]+)/$', GalleryDetailView.as_view(), name='pl-gallery'),
-    # Following is user generated to alter behavior:
     url(r'^gallery/(?P<pk>[\-\d\w]+)/$', 'photologue.views.gallery_view', name='pl-gallery'),
     url(r'^gallery/page/(?P<page>[0-9]+)/$', GalleryListView.as_view(), name='pl-gallery-list'),
-
-    # Following was the original:
-    # url(r'^gallery/page/(?P<page>[0-9]+)/$', GalleryListView.as_view(), name='pl-gallery-list'),
-
     url(r'^photo/(?P<photo_id>\d+)/edit_photo/$', 'photologue.views.edit_photo'),
     url(r'^photo/(?P<photo_id>\d+)/delete_photo/$', 'photologue.views.delete_photo', name='delete_photo'),
     url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$',
@@ -47,17 +40,9 @@ urlpatterns = patterns(
     url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
         PhotoMonthArchiveView.as_view(),
         name='pl-photo-archive-month'),
-    # url(r'^photo/(?P<year>\d{4})/$',
-    #     PhotoYearArchiveView.as_view(),
-    #     name='pl-photo-archive-year'),
     url(r'^photo/$',
         PhotoArchiveIndexView.as_view(),
         name='pl-photo-archive'),
-    # Photologue original
-    # url(r'^photo/(?P<pk>[\-\d\w]+)/$',
-    #     PhotoDetailView.as_view(),
-    #     name='pl-photo'),
-    # User generate to alter behavior
     url(r'^photo/(?P<pk>[\-\d\w]+)/$',
         'photologue.views.photo_view',
         name='pl-photo'),
