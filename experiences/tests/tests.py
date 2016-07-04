bab5dcb70724f7f97f17e36be84c8ff24389f869
asgaines <andrew.s.gaines@gmail.com>
2016-07-04 16:20:03 -0600

    Add is_public=True and broke line

--- experiences/tests/tests.py
+++ experiences/tests/tests.py
@@ -10,7 +10,8 @@ class ExperienceTest(TestCase):
 
     def setUp(self):
         self.explorer = helpers.new_explorer()
-        self.experience = Experience.objects.create(experience=self.experience_string, author=self.explorer)
+        self.experience = Experience.objects.create(experience=self.experience_string,
+                author=self.explorer, is_public=True)
 
     def test_experience_created(self):
         self.assertEqual(self.experience_string, str(self.experience))
