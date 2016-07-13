633038cd9df354a01e9498dbaf7de59fca03923d
asgaines <andrew.s.gaines@gmail.com>
2016-07-13 13:21:42 -0600

    Add tests to examine public/private latest narrative

--- experiences/tests/tests.py
+++ experiences/tests/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
 from experiences.models import Experience
+from narratives.models import Narrative
 from experiences.forms import ExperienceForm
 from explorers.tests import helpers
 
 
 class ExperienceTest(TestCase):
-    experience_string = 'Walk on the moon'
+    experience_string = 'Walk on the Moon'
 
     def setUp(self):
         self.explorer = helpers.new_explorer()
@@ -27,3 +28,22 @@ class ExperienceTest(TestCase):
         experience = exp_form.save()
         self.assertTrue(experience in self.explorer.experiences.all())
         self.assertTrue(experience.author == self.explorer)
+
+
+    def test_latest_narratives(self):
+        narr1 = Narrative.objects.create(experience=self.experience,
+                author=self.explorer, narrative='''There were two astronauts
+                lucky enough to make it into space today!''')
+        self.assertTrue(self.experience.public_narratives().count() == 1)
+        self.assertTrue(self.experience.narratives.count() == 1)
+        self.assertEqual(self.experience.latest_narrative(), narr1)
+        self.assertEqual(self.experience.latest_public_narrative(), narr1)
+
+        narr2 = Narrative.objects.create(experience=self.experience,
+                author=self.explorer, narrative='''There was a full moon
+                tonight''', is_public=False)
+        self.assertTrue(self.experience.public_narratives().count() == 1)
+        self.assertTrue(self.experience.narratives.count() == 2)
+        self.assertEqual(self.experience.latest_narrative(), narr2)
+        self.assertEqual(self.experience.latest_public_narrative(), narr1)
+
