d9f898f39fe503f059ce88616b1166b806d1d463
asgaines <andrew.s.gaines@gmail.com>
2016-07-23 20:37:02 -0600

    Momentary changes

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,15 @@
 from fabric.api import local
-from fabric.api import lcd
+from fabric.api import cd, run
 
 def prepare_deployment(branch_name):
     local('python manage.py test')
     local('git checkout master && git merge ' + branch_name)
+    local('git push origin')
 
 def deploy():
-    with lcd('interpopulus@interpopulus.webfactional.com:/home/interpopulus/webapps/acressity/acressity'):
-        local('git pull /home/asgaines/Documents/Code/Websites/acressity')
-        local('python manage.py makemigrations')
-        local('python manage.py migrate')
-        local('python manage.py test')
+    with cd('/home/interpopulus/webapps/acressity/acressity'):
+        #run('git fetch origin')
+        #run('python manage.py makemigrations')
+        run('python manage.py migrate')
+        #run('./manage.py test')
+        #run('./webapps/acressity/apache/bin/restart')
